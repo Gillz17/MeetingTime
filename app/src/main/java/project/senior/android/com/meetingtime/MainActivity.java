da368c4b3fadab1edbb99792dc089b2303d4b6ce
gillz17 <zmc17@ptd.net>
2017-08-21 22:07:18 -0400

    Changed bottom nav colors

--- app/src/main/java/project/senior/android/com/meetingtime/MainActivity.java
+++ app/src/main/java/project/senior/android/com/meetingtime/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CalendarView;
 import android.widget.TextView;
 
 
