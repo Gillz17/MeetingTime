8b568e45c63b33b77a6c4a6fc8724eb63af0790c
gillz17 <zmc17@ptd.net>
2017-09-16 13:53:27 -0400

    Sign in and register pages up. Need to hook up buttons next to actually do something.

--- app/src/main/java/project/senior/android/com/meetingtime/LoginActivity.java
+++ app/src/main/java/project/senior/android/com/meetingtime/LoginActivity.java
@@ -1,26 +1,34 @@
 package project.senior.android.com.meetingtime;
 
+import android.content.Intent;
+import android.drm.ProcessedData;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.firebase.analytics.FirebaseAnalytics;
-
 public class LoginActivity extends AppCompatActivity{
-    private FirebaseAnalytics mFirebaseAnalytics;
-    private ImageView logo;
-    private TextView message;
-    private Button signin;
-    private Button register;
+
 
     @Override
     protected void onCreate(final Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
+        final EditText tfUsername = (EditText) findViewById(R.id.tfUsername);
+        final EditText tfPassword = (EditText) findViewById(R.id.tfPassword);
+        final Button bSignin = (Button) findViewById(R.id.bSignin);
+        final TextView registerLink = (TextView) findViewById(R.id.tvRegister);
+
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
 
     }
 }
--- app/src/main/java/project/senior/android/com/meetingtime/RegisterActivity.java
+++ app/src/main/java/project/senior/android/com/meetingtime/RegisterActivity.java
@@ -1,7 +1,12 @@
 package project.senior.android.com.meetingtime;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final EditText tfName = (EditText) findViewById(R.id.tfName);
+        final EditText tfUsername = (EditText) findViewById(R.id.tfUsername);
+        final EditText tfPassword = (EditText) findViewById(R.id.tfPassword);
+        final Button bRegister = (Button) findViewById(R.id.bRegister);
+
+        final TextView signinLink = (TextView) findViewById(R.id.tvSignin);
+
+        signinLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signinIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+                RegisterActivity.this.startActivity(signinIntent);
+            }
+        });
+
+
+
     }
 }
