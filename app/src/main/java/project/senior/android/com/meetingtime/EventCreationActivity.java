d8262c6699b555c4ed8c3cb8bb67fa83d620d5b7
gillz17 <zmc17@ptd.net>
2017-11-07 23:16:28 -0500

    EventCreation gets the start and end time entered by the user

--- app/src/main/java/project/senior/android/com/meetingtime/EventCreationActivity.java
+++ app/src/main/java/project/senior/android/com/meetingtime/EventCreationActivity.java
@@ -1,6 +1,7 @@
 package project.senior.android.com.meetingtime;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.sql.Time;
 import java.util.Calendar;
 
 /**
@@ -25,7 +27,9 @@ public class EventCreationActivity extends AppCompatActivity{
     private TextView date;
     private DatePickerDialog.OnDateSetListener mDateSetListener;
     private TextView startTime;
+    private TimePickerDialog.OnTimeSetListener mStartOnTimeSetListener;
     private TextView endTime;
+    private TimePickerDialog.OnTimeSetListener mEndOnTimeSetListener;
     private EditText location;
     private RadioButton red;
     private RadioButton blue;
@@ -80,6 +84,88 @@ public class EventCreationActivity extends AppCompatActivity{
                 String Date = month + "/" + day + "/" + year;
                 date.setText(Date);
 
+            }
+        };
+        startTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int hour = cal.get(Calendar.HOUR_OF_DAY);
+                int minute = cal.get(Calendar.MINUTE);
+
+                boolean is24HourView = false;
+
+                TimePickerDialog dialog = new TimePickerDialog(
+                        EventCreationActivity.this,
+                        android.R.style.ThemeOverlay_Material_Dialog_Alert,
+                        mStartOnTimeSetListener,
+                        hour, minute, is24HourView);
+                dialog.show();
+            }
+        });
+        mStartOnTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+                String AmOrPM = "AM";
+                if(hour == 0){
+                    hour = 12;
+                    AmOrPM = "AM";
+                }
+                if(hour > 11){
+                    hour = hour - 12;
+                    AmOrPM = "PM";
+                }
+                String minutes;
+                if(minute == 0){
+                    minutes = "00";
+                }else if(minute < 10) {
+                    minutes = "0" + minute;
+                }else{
+                    minutes = String.valueOf(minute);
+                }
+                String time = hour + ":" + minutes + AmOrPM;
+                startTime.setText(time);
+            }
+        };
+        endTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int hour = cal.get(Calendar.HOUR_OF_DAY);
+                int minute = cal.get(Calendar.MINUTE);
+
+                boolean is24HourView = false;
+
+                TimePickerDialog dialog = new TimePickerDialog(
+                        EventCreationActivity.this,
+                        android.R.style.ThemeOverlay_Material_Dialog_Alert,
+                        mEndOnTimeSetListener,
+                        hour, minute, is24HourView);
+                dialog.show();
+            }
+        });
+        mEndOnTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+                String AmOrPM = "AM";
+                if(hour == 0){
+                    hour = 12;
+                    AmOrPM = "AM";
+                }
+                if(hour > 11){
+                    hour = hour - 12;
+                    AmOrPM = "PM";
+                }
+                String minutes;
+                if(minute == 0){
+                    minutes = "00";
+                }else if(minute < 10) {
+                    minutes = "0" + minute;
+                }else{
+                    minutes = String.valueOf(minute);
+                }
+                String time = hour + ":" + minutes + AmOrPM;
+                endTime.setText(time);
             }
         };
     }
