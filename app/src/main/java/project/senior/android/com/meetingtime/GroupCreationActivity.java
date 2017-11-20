228b8396f33fcfee3519d263e1accdd3bb001c10
Gillz17 <zmc17@ptd.net>
2017-11-20 18:43:57 -0500

    Groups page gets list of users to add to group.

--- app/src/main/java/project/senior/android/com/meetingtime/GroupCreationActivity.java
+++ app/src/main/java/project/senior/android/com/meetingtime/GroupCreationActivity.java
@@ -4,20 +4,78 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import android.widget.SearchView;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by NCASE on 11/19/2017.
  */
 
 public class GroupCreationActivity extends AppCompatActivity{
-    private SearchView mSearchView;
-    private RecyclerView mRecyclerView;
+    private ListView mListView;
+    private Button mCreate;
 
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    final String UUID = user.getUid();
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference mUsers = database.getReference("users");
+
+    List<String> listUsers;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_groups);
+
+        mListView = (ListView) findViewById(R.id.listUsers);
+        mCreate = (Button) findViewById(R.id.bCreateGroup);
+        listUsers = new ArrayList<>();
+
+        getUserGroups(UUID);
+
+        mCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //ADD TO SEND DATA TO DATABASE
+            }
+        });
+    }
+
+
+    public void getUserGroups(String UUID) {
+        mUsers.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                listUsers.clear();
+                for(DataSnapshot child : dataSnapshot.getChildren()){
+                    HashMap<String, String> value = (HashMap<String, String>)child.getValue();
+                    String user = value.get("name");
+                    listUsers.add(user);
+                }
+                ListAdapter adapter = new ArrayAdapter<>(GroupCreationActivity.this,
+                        android.R.layout.simple_list_item_1, listUsers);
+                mListView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
